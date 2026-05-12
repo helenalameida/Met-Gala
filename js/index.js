cdd30fc9e02fd0bfc71e31ef572f54349a240273
Maria Helena <40381@raphaeldisanto.com.br>
2026-05-12 08:02:15 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//Lista de looks do Met Gala com nome e imagem local
+const metGala - [
+    {nome: "Rihanna no Met Gala 2026", imagem: "img/metgala1.jpg"},
+    {nome: "Lisa no Met Gala 2026", imagem: "img/metgala2.jpg"},
+    {nome: "Jisso no Met Gala 2026", imagem: "img/metgala3.jpg"},
+    {nome: "Jennie no Met Gala 2026", imagem: "img/metgala4.jpg"},
+    {nome: "Rose no Met Gala 2026", imagem: "img/metgala5.jpg"},
+    {nome: "Beyoncé no Met Gala 2026", imagem: "img/metgala6.jpg"},
+    {nome: "Karina no Met Gala 2026", imagem: "img/metgala7.jpg"},
+    {nome: "NingNing no Met Gala 2026", imagem: "img/metgala8.jpg"},
+];
+//começa os votos zerados
+const votos = new Array(metGala.length).fill(0);
